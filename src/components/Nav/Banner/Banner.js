fe26ead2ceb510f07584b3d4e0cae31e35f1dfd9
Rusweld <sanekoir2015@gmail.com>
2022-10-13 19:18:07 +0300

    implement banner

--- src/components/Nav/Banner/Banner.js
+++ src/components/Nav/Banner/Banner.js
@@ -1,7 +1,29 @@
 import React from "react";
-
+import "./Banner.css";
 function Banner() {
-  return <div>Banner</div>;
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundPosition: "center center",
+        backgroundSize: "cover",
+        backgroundColor: "black",
+        // backgroundImage:
+        //   'url("https://assets.nflxext.com/ffe/siteui/vlv3/28b69a57-cadf-43d9-8a95-e5f2e11199de/1e83ca74-1157-4714-98a1-38e052114cd4/BY-ru-20221010-popsignuptwoweeks-perspective_alpha_website_small.jpg")',
+      }}
+    >
+      <div className="banner__content">
+        <h1 className="banner__title">Movie Name</h1>
+        <div className="banner__buttons">
+          <button className="banner__button">Play</button>
+          <button className="banner__button">My List</button>
+        </div>
+        <h1 className="bunner__description">this is test description</h1>
+      </div>
+
+      <div className="banner--fade-bottom"></div>
+    </header>
+  );
 }
 
 export default Banner;
--- src/pages/HomeScreen/HomeScreen.js
+++ src/pages/HomeScreen/HomeScreen.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Nav from "../../components/Nav";
+import Banner from "../../components/Nav/Banner";
 import "./HomeScreen.css";
 
 function HomeScreen() {
   return (
     <div className="homescreen">
       <Nav />
-
       <Banner />
       {/* Row */}
     </div>
